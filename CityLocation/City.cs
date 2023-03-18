08096837ad1966c5a2fbba45da78dc944696838c
BeskonchinRV <BeskonchinRV@411D-2>
2023-03-18 12:07:13 +0700

    Добавьте файлы проекта.

---
+++ CityLocation/City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CityLocation
+{
+    class City
+    {
+        public string Name { get; set; }
+        public GeoLocation Location { get; set; }
+
+    }
+}
